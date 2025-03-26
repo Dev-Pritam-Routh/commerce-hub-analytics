921525c9a52fe6683e5476e7b321ea305a496f3c
Pritam Routh <pritamroouth67123@gmail.com>
2025-03-26 16:26:26 +0530

    none

--- src/server/server.js
+++ src/server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB URI from environment variables
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce-app';
+const MONGODB_URI = "mongodb+srv://pritamrouth2003:FUsM0dNuQo2Qaxft@cluster0.kf6y8.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0";
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
